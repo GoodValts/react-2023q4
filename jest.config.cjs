02fdc6f6f2998dcac7d4fa9ed4d83efc803e33c0
Martynjuk Ivan <martiniukgomel@gmail.com>
2023-11-15 17:05:13 +0300

    fix: resolver on jest.config 15 ноя 2023 г. 17:05:13

--- jest.config.cjs
+++ jest.config.cjs
@@ -9,7 +9,7 @@ module.exports = {
   moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|css|scss|less)$':
-      '<rootDir>/__mocks__/fileMock.{j,t}s',
+      '<rootDir>/mocks/styleMock.json',
   },
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}', '!src/**/*.d.ts'],
